9ef70c10975e93292a085263308ebc5715249f5e
Raza2715 <razasd7506@gmail.com>
2025-05-25 23:59:42 +0400

    Initial commit

---
+++ app/Dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+ return <p> <strong>Dashboard Page </strong></p>;
+}
+
+// export default function Page() {
+//   return <p>Customers Page</p>;
+// }
---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+
+import '@/app/ui/global.css';
+import {inter} from '@/app/ui/Fonts';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}antialiased`}>{children}</body>
+    </html>
+  );
+}
+
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
